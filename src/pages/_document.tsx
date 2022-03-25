6bba8fe2578de89780ce68a04c0119461c546938
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-03-25 09:47:28 -0300

    algumas coisas com problema hehe

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,19 @@
 import { Head, Html, Main, NextScript } from "next/document"
+import Document, { DocumentContext } from 'next/document'
 
-export default function Document() {
-    return (
+class MyDocument extends Document {
+//   static async getInitialProps(ctx: DocumentContext) {
+//     const initialProps = await Document.getInitialProps(ctx)
+
+//     return initialProps
+//   }
+    render() {
+        return (
         <Html>
             <Head>
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
+                <link rel="shortcut icon" href="/favicon.png" type="image/png" />
             </Head>
             <body>
                 <Main />
@@ -14,3 +22,25 @@ export default function Document() {
         </Html>
     )
 }
+
+}
+
+export default MyDocument
+
+
+
+// export default function Document() {
+//     return (
+//         <Html>
+//             <Head>
+//                 <link rel="preconnect" href="https://fonts.googleapis.com" />
+//                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"anonymous"} />
+//                 <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+//             </Head>
+//             <body>
+//                 <Main />
+//                 <NextScript />
+//             </body>
+//         </Html>
+//     )
+// }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/home.module.scss'
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Ignews</title>
-      </Head>
-      <div className={styles.title}>
-        <h1>Hello World</h1>
-      </div>
-
+      <header>
+        <title>Ignews</title>     
+      </header>
+    <body>
+      Hello World
+    </body>
     </>)
 }
 
